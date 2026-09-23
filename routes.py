c9cfb2b1b0a139ba94649df714c9a3a21c6ae458
Gayathridevi5533 <gayathrideviganesh5533@gmail.com>
2026-09-23 12:31:49 +1200

    changes in loginpage - css

--- routes.py
+++ routes.py
@@ -439,7 +439,7 @@ def login():
         else:
             error = "Invalid username or password."
 
-    return render_template("login.html", error=error)
+    return render_template("loginpage.html", error=error)
 
 
 @routes.route("/logout")
